0bd74b31664c120d927690a8148f9926c7dc7cc5
mafintosh <mathiasbuus@gmail.com>
2012-12-08 16:37:39 +0100

    added example

--- example.js
+++ example.js
@@ -1,26 +1,26 @@
-var rs = require('request-stream');
+var cs = require('conversation-stream');
 var net = require('net');
 
 net.createServer(function(socket) {
-	var r = rs();
+	var conversation = cs();
 
-	socket.pipe(r).pipe(socket);
+	socket.pipe(conversation).pipe(socket);
 
-	r.on('request', function(request, respond) {
-		r.request({server:request}, respond);
+	conversion.on('message', function(message, respond) {
+		conversion.send({server:message}, respond);
 	});
 
 }).listen(9585, function() {
 	var socket = net.connect(9585);
-	var r = rs();
+	var conversion = cs();
 
-	socket.pipe(r).pipe(socket);
+	socket.pipe(conversion).pipe(socket);
 
-	r.on('request', function(request, respond) {
-		respond(null, {client:request});
+	conversion.on('message', function(message, respond) {
+		respond(null, {client:message});
 	});
 
-	r.request({hello:'world'}, function(err, reply) {
+	conversion.send({hello:'world'}, function(err, reply) {
 		console.log(reply);
 	});
 });
